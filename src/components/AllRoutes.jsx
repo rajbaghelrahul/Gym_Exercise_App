560ea2cb855322979a031ab49e40e711dbb93281
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-04-13 18:47:35 +0530

    Gym Exercises App

---
+++ src/components/AllRoutes.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import ExerciseDetail from "../pages/ExerciseDetail";
+import NotFound from "../pages/NotFound";
+import { Box } from "@mui/material";
+
+const AllRoutes = () => {
+  return (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      {/* <h1>Hello World!</h1> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
+
+export default AllRoutes;
---
+++ src/utils/fetchData.jsx
@@ -0,0 +1,24 @@
+export const exerciseOptions = {
+    method: 'GET',
+    // url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+        'X-RapidAPI-Key': '3bec4ca188msh2259d78f7d00ee0p11f8c8jsn111b97ff3d12',
+        // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    },    
+};
+
+export const youtubeOptions = {
+    method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3bec4ca188msh2259d78f7d00ee0p11f8c8jsn111b97ff3d12',
+		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+	}
+}
+
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+}
