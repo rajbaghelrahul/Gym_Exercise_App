868a6c6ed9ac2090eb4db59dc0785c838ed38079
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-04-17 22:06:47 +0530

    Componenets are updated

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -7,7 +7,8 @@ import Loader from './Loader';
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
   <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
     <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
-      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises {targetMuscleExercises.length}
+      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises 
+      {/* {targetMuscleExercises.length} */}
     </Typography>
     <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
       {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
